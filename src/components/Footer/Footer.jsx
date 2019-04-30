9616816b9084b0f70b2ae4c7da76fee051e5ade0
taha <haqtaha@gmail.com>
2019-04-30 20:46:56 +0500

    changed name

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -40,7 +40,7 @@ function Footer({ ...props }) {
           <span>
             &copy; {1900 + new Date().getYear()}{" "}
             <a href="https://www.creative-tim.com" className={classes.a}>
-              Project Deosai
+              Xord.id
             </a>, made with love for a better web
           </span>
         </p>
